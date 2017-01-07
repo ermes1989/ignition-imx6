7380ae2ac66812061f57e8eaa34316425f8eaafc
Marco Servillo <marco.servillo@alten.it>
2017-01-07 17:44:44 +0100

    Fix OpenELEC 7.0 on Cubox

--- OpenELEC.tv/install.sh
+++ OpenELEC.tv/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo "Downloading and installing"
-curl -L -k http://releases.openelec.tv/OpenELEC-imx6.arm-7.0.0.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
+curl -L -k http://snapshots.openelec.tv/OpenELEC-imx6.arm-7.0-devel-20161231135043-r22956-g4da0fcd.img.gz --progress | gunzip | dd of=/dev/mmcblk0 bs=1M conv=fsync
 sync
